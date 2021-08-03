b59ecd2876df76c820e3bf4291ccf360f29da58b
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2021-08-03 10:15:30 -0700

    section 2.5 updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,47 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import Blog from "../views/Blog.vue";
+import BlogPage from "../views/BlogPage.vue";
+import HelloPage from "../views/HelloPage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "event-list",
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/event/:id",
+    name: "event-show",
+    component: EventShow,
+    props: true,
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate,
+  },
+  {
+    path: "/blog",
+    name: "blog",
+    component: Blog,
+  },
+  {
+    path: "/blog/:id",
+    name: "blog-show",
+    component: BlogPage,
+    props: true,
+  },
+  {
+    path: "/hello/:name1/:name2",
+    name: "hello-page",
+    component: HelloPage,
+    props: true,
   },
 ];
 
---
+++ src/views/EventShow.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Showing event #{{ id }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ["id"],
+};
+</script>
